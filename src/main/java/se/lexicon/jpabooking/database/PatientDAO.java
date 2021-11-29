a1390592bda81dca1c0d5eb6c80c6748c5d6380c
Erik Svensson <eosvensson@gmail.com>
2021-11-29 13:25:30 +0100

    Updated PatientDAO to extend DAOGenericCRUD

--- src/main/java/se/lexicon/jpabooking/database/PatientDAO.java
+++ src/main/java/se/lexicon/jpabooking/database/PatientDAO.java
@@ -5,11 +5,7 @@ import se.lexicon.jpabooking.model.Patient;
 import java.util.List;
 import java.util.Optional;
 
-public interface PatientDAO {
-    Patient save(Patient patient);
-    Optional<Patient> findById(String id);
+public interface PatientDAO extends DAOGenericCRUD<Patient, String>{
     Optional<Patient> findByPnr(String pnr);
-    List<Patient> findAll();
     List<Patient> searchByName(String name);
-    void delete(String id);
 }
