b0358e3d98200bfed78fa4f198937825ac15b8f4
Erik Svensson <eosvensson@gmail.com>
2021-11-29 16:24:52 +0100

    Added Booking

---
+++ src/main/java/se/lexicon/jpabooking/model/Booking.java
@@ -0,0 +1,114 @@
+package se.lexicon.jpabooking.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import static se.lexicon.jpabooking.model.constants.EntityConstants.GENERATOR;
+import static se.lexicon.jpabooking.model.constants.EntityConstants.UUID_GENERATOR;
+
+@Entity
+public class Booking {
+
+    @Id
+    @GeneratedValue(generator = GENERATOR)
+    @GenericGenerator(name = GENERATOR, strategy = UUID_GENERATOR)
+    @Column(updatable = false)
+    private String id;
+    private LocalDateTime dateTime;
+    private BigDecimal price;
+    private String administratorId;
+    private String vaccineType;
+    private boolean vacant;
+
+    public Booking(String id, LocalDateTime dateTime, BigDecimal price, String administratorId, String vaccineType, boolean vacant) {
+        this.id = id;
+        this.dateTime = dateTime;
+        this.price = price;
+        this.administratorId = administratorId;
+        this.vaccineType = vaccineType;
+        this.vacant = vacant;
+    }
+
+    public Booking() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getAdministratorId() {
+        return administratorId;
+    }
+
+    public void setAdministratorId(String administratorId) {
+        this.administratorId = administratorId;
+    }
+
+    public String getVaccineType() {
+        return vaccineType;
+    }
+
+    public void setVaccineType(String vaccineType) {
+        this.vaccineType = vaccineType;
+    }
+
+    public boolean isVacant() {
+        return vacant;
+    }
+
+    public void setVacant(boolean vacant) {
+        this.vacant = vacant;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Booking booking = (Booking) o;
+        return vacant == booking.vacant && Objects.equals(id, booking.id) && Objects.equals(dateTime, booking.dateTime) && Objects.equals(price, booking.price) && Objects.equals(administratorId, booking.administratorId) && Objects.equals(vaccineType, booking.vaccineType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, dateTime, price, administratorId, vaccineType, vacant);
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "id='" + id + '\'' +
+                ", dateTime=" + dateTime +
+                ", price=" + price +
+                ", administratorId='" + administratorId + '\'' +
+                ", vaccineType='" + vaccineType + '\'' +
+                ", vacant=" + vacant +
+                '}';
+    }
+}
