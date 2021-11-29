fe9d9b3d514267694c50b97749903cb429ab72bc
Erik Svensson <eosvensson@gmail.com>
2021-11-29 10:01:33 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/jpabooking/JpaBookingApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.jpabooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/jpabooking/JpaBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.jpabooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
