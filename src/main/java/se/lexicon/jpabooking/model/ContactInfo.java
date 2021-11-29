7f41d35b253b190974c7ffc8a5994cc10ab2ac95
Erik Svensson <eosvensson@gmail.com>
2021-11-29 13:03:58 +0100

    Added ContactInfo

---
+++ src/main/java/se/lexicon/jpabooking/model/ContactInfo.java
@@ -0,0 +1,80 @@
+package se.lexicon.jpabooking.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+import static se.lexicon.jpabooking.model.constants.EntityConstants.GENERATOR;
+import static se.lexicon.jpabooking.model.constants.EntityConstants.UUID_GENERATOR;
+
+@Entity
+public class ContactInfo {
+
+    @Id
+    @GeneratedValue(generator = GENERATOR)
+    @GenericGenerator(name = GENERATOR, strategy = UUID_GENERATOR)
+    @Column(updatable = false)
+    private String id;
+    @Column(unique = true)
+    private String email;
+    private String phone;
+
+    public ContactInfo(String id, String email, String phone) {
+        this.id = id;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public ContactInfo() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContactInfo that = (ContactInfo) o;
+        return Objects.equals(id, that.id) && Objects.equals(email, that.email) && Objects.equals(phone, that.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, phone);
+    }
+
+    @Override
+    public String toString() {
+        return "ContactInfo{" +
+                "id='" + id + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
