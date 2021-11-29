1f246faafb4ee991619b2d31a215d0763f9e5dd4
Erik Svensson <eosvensson@gmail.com>
2021-11-29 16:05:04 +0100

    Entity Premises added

---
+++ src/main/java/se/lexicon/jpabooking/model/Premises.java
@@ -0,0 +1,68 @@
+package se.lexicon.jpabooking.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+import static se.lexicon.jpabooking.model.constants.EntityConstants.GENERATOR;
+import static se.lexicon.jpabooking.model.constants.EntityConstants.UUID_GENERATOR;
+
+@Entity
+public class Premises {
+
+    @Id
+    @GeneratedValue(generator = GENERATOR)
+    @GenericGenerator(name = GENERATOR, strategy = UUID_GENERATOR)
+    @Column(updatable = false)
+    private String id;
+    private String name;
+
+    public Premises(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Premises() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Premises premises = (Premises) o;
+        return Objects.equals(id, premises.id) && Objects.equals(name, premises.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Premises{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
