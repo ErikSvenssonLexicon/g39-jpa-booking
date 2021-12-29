29a3c579a1c9d65a54314de9f4cd81ed21cadf40
Erik Svensson <eosvensson@gmail.com>
2021-12-29 09:53:08 +0100

    Enables method security in BookingSecurityConfiguration with prePost

--- src/main/java/se/lexicon/jpabooking/security/BookingSecurityConfiguration.java
+++ src/main/java/se/lexicon/jpabooking/security/BookingSecurityConfiguration.java
@@ -2,12 +2,16 @@ package se.lexicon.jpabooking.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true
+)
 public class BookingSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final AppUserAuthenticationSuccessHandler appUserAuthenticationSuccessHandler;
