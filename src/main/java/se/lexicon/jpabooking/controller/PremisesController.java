71c897d40d12ec9eb98fc3d3f77388fa0ecfdef7
Erik Svensson <eosvensson@gmail.com>
2022-01-18 15:57:43 +0100

    Secured PremisesController

--- src/main/java/se/lexicon/jpabooking/controller/PremisesController.java
+++ src/main/java/se/lexicon/jpabooking/controller/PremisesController.java
@@ -2,6 +2,7 @@ package se.lexicon.jpabooking.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import se.lexicon.jpabooking.model.dto.form.BookingForm;
@@ -23,17 +24,20 @@ public class PremisesController {
         this.premisesService = premisesService;
     }
 
+    @Secured({"ROLE_SUPER_ADMIN", "ROLE_PREMISES_ADMIN"})
     @PostMapping("/api/v1/premises")
     public ResponseEntity<PremisesDTO> create(@Validated(OnPost.class) @RequestBody PremisesForm form){
         PremisesDTO premises = premisesService.create(form);
         return ResponseEntity.status(201).body(premises);
     }
 
+    @Secured({"ROLE_SUPER_ADMIN", "ROLE_PREMISES_ADMIN"})
     @PostMapping("/api/v1/premises/{id}/bookings")
     public ResponseEntity<PremisesDTO> addBooking(@PathVariable("id") String id, @Validated(OnPost.class) @RequestBody BookingForm form){
         return ResponseEntity.ok(premisesService.addNewBooking(id, form));
     }
 
+    @Secured({"ROLE_SUPER_ADMIN", "ROLE_PREMISES_ADMIN"})
     @GetMapping("/api/v1/premises")
     public ResponseEntity<List<PremisesDTO>> find(){
         return ResponseEntity.ok(
@@ -46,11 +50,13 @@ public class PremisesController {
         return ResponseEntity.ok(premisesService.findById(id));
     }
 
+    @Secured({"ROLE_SUPER_ADMIN", "ROLE_PREMISES_ADMIN"})
     @PutMapping("/api/v1/premises/{id}")
     public ResponseEntity<PremisesDTO> update(@PathVariable("id") String id, @Validated(OnPut.class) @RequestBody PremisesForm form){
         return ResponseEntity.ok(premisesService.update(id, form));
     }
 
+    @Secured({"ROLE_SUPER_ADMIN", "ROLE_PREMISES_ADMIN"})
     @DeleteMapping("/api/v1/premises/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") String id){
         premisesService.delete(id);
