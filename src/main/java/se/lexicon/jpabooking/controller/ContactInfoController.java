3c9d6ec589971e260af6cd42cc0c75de2e040f8f
Erik Svensson <eosvensson@gmail.com>
2022-01-18 15:51:54 +0100

    Secured ContactInfoController

--- src/main/java/se/lexicon/jpabooking/controller/ContactInfoController.java
+++ src/main/java/se/lexicon/jpabooking/controller/ContactInfoController.java
@@ -1,6 +1,8 @@
 package se.lexicon.jpabooking.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import se.lexicon.jpabooking.model.dto.form.ContactInfoForm;
@@ -20,11 +22,13 @@ public class ContactInfoController {
         this.contactInfoService = contactInfoService;
     }
 
+    @Secured({"ROLE_SUPER_ADMIN"})
     @PostMapping("/api/v1/contact")
     public ResponseEntity<ContactInfoDTO> create(@Validated(OnPost.class) @RequestBody ContactInfoForm contactInfoForm){
         return ResponseEntity.status(201).body(contactInfoService.create(contactInfoForm));
     }
 
+    @Secured({"ROLE_SUPER_ADMIN"})
     @GetMapping("/api/v1/contact")
     public ResponseEntity<List<ContactInfoDTO>> find(){
         return ResponseEntity.ok(contactInfoService.findAll());
@@ -35,11 +39,13 @@ public class ContactInfoController {
         return ResponseEntity.ok(contactInfoService.findById(id));
     }
 
+    @PreAuthorize("#id == authentication.principal.contactId || hasAnyRole('SUPER_ADMIN')")
     @PutMapping("/api/v1/contact/{id}")
     public ResponseEntity<ContactInfoDTO> update(@PathVariable("id") String id, @Validated(OnPut.class) @RequestBody ContactInfoForm contactInfoForm){
         return ResponseEntity.ok(contactInfoService.update(id, contactInfoForm));
     }
 
+    @Secured({"ROLE_SUPER_ADMIN"})
     @DeleteMapping("/api/v1/contact/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") String id){
         contactInfoService.delete(id);
